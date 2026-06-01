c2a51d149075e80790b859df33d6e52808039ecd
prhndk123 <prihandikavahryansyah@gmail.com>
2026-06-01 19:01:54 +0700

    fix: update nitro configuration to boolean in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     // nitro/vite builds from this
     server: { entry: "server" },
   },
-  nitro: { preset: "vercel" },
+  nitro: true,
   vite: {
     plugins: [
       VitePWA({
